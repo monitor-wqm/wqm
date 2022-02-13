2303b694472ce219620b9e6c49aea6677f201ceb
于2002 <yuhaoyong2002@163.com>
2022-02-13 21:59:51 +0800

    监测点名查询，R对象封装

--- src/main/java/com/water/quality/controller/MonitorPointController.java
+++ src/main/java/com/water/quality/controller/MonitorPointController.java
@@ -1,6 +1,7 @@
 package com.water.quality.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.water.quality.asserts.Assert;
 import com.water.quality.pojo.entity.MonitorPointEntity;
 import com.water.quality.r.R;
@@ -8,8 +9,18 @@ import com.water.quality.r.enums.ResponseEnum;
 import com.water.quality.service.MonitorPointService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -58,30 +69,43 @@ public class MonitorPointController {
      * 监测点更新
      */
     @PutMapping("/update")
-    public Boolean update(@RequestBody MonitorPointEntity monitorPoint){
+    public R update(@RequestBody MonitorPointEntity monitorPoint){
         //todo 对更新数据进行判断 判重、判改变数据的影响、判新数据时候符合要求
-        return monitorPointService.updateById(monitorPoint);
+        boolean update = monitorPointService.updateById(monitorPoint);
+        Assert.isTrue(update, ResponseEnum.UPDATE_DATA_ERROR);
+        return R.ok();
     }
 
     /**
      * 监测点删除
      */
     @DeleteMapping("/delete/{id}")
-    public Boolean delete(@PathVariable Integer id){
+    public R delete(@PathVariable Integer id){
         //todo 对删除数据进行判断 是否影响原有数据
-        return monitorPointService.removeById(id);
+        boolean delete = monitorPointService.removeById(id);
+        Assert.isTrue(delete , ResponseEnum.DELETE_DATA_ERROR);
+        return R.ok();
     }
 
     /**
-     * 监测点查询
+     * 监测点id查询
      */
-    @GetMapping("/get/{id}")
-    public MonitorPointEntity getById(@PathVariable Integer id){
-        return monitorPointService.getById(id);
+    @GetMapping("/search/monitorPoint/{id}")
+    public R getById(@PathVariable Integer id){
+        MonitorPointEntity getById = monitorPointService.getById(id);
+        Assert.notNull(getById, ResponseEnum.DATABASE_NULL_ERROR);
+        return R.ok().data("monitorPointId", getById);
     }
 
     /**
-     * 监测点名查询 等等
+     * 监测点名查询
      */
+    @GetMapping("/search/monitorPoint/{name}")
+    public R searchMonitorPoint(@PathVariable("name") @ApiParam(value = "监测点名", required = true) String name) {
+        MonitorPointEntity monitorPointEntity = monitorPointService.getOne(new QueryWrapper<MonitorPointEntity>()
+                .eq("name",name));
+        Assert.notNull(monitorPointEntity, ResponseEnum.DATABASE_NULL_ERROR);
+        return R.ok().data("monitorPointName",monitorPointEntity );
+    }
 }
 
--- src/main/java/com/water/quality/controller/UserController.java
+++ src/main/java/com/water/quality/controller/UserController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RestController
 @RequestMapping("/api/user")
-public class UserController {
+public class  UserController {
 
     @Autowired
     private UserService userService;
